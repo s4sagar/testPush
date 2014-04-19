2f16ced5adf01c86242d34ae11925fabb7da5bd4
Vidya Sagar Sankaranarayanan <vidya.sagar@bs-shipmanagement.com>
2014-04-19 22:52:38 +0530

    Test Push

---
+++ platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,46 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/org.jboss.aerogear.cordova.push/www/aerogear-push.js",
+        "id": "org.jboss.aerogear.cordova.push.AeroGear.UnifiedPush",
+        "clobbers": [
+            "push"
+        ]
+    },
+    {
+        "file": "plugins/com.phonegap.plugins.PushPlugin/www/PushNotification.js",
+        "id": "com.phonegap.plugins.PushPlugin.PushNotification",
+        "clobbers": [
+            "PushNotification"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/Coordinates.js",
+        "id": "org.apache.cordova.geolocation.Coordinates",
+        "clobbers": [
+            "Coordinates"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/PositionError.js",
+        "id": "org.apache.cordova.geolocation.PositionError",
+        "clobbers": [
+            "PositionError"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/Position.js",
+        "id": "org.apache.cordova.geolocation.Position",
+        "clobbers": [
+            "Position"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/geolocation.js",
+        "id": "org.apache.cordova.geolocation.geolocation",
+        "clobbers": [
+            "navigator.geolocation"
+        ]
+    }
+]
+});
---
+++ plugins/org.jboss.aerogear.cordova.push/README.md
@@ -0,0 +1,5 @@
+# AeroGear PushPlugin Cordova
+> This plugin makes starting with AeroGear Unified Push simple.
+
+## Getting Started 
+* [AeroGear Push Plugin Guide](http://aerogear.org/docs/guides/aerogear-cordova/AerogearCordovaPush/)
